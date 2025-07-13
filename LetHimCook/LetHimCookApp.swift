0e77d6edbad5a4643ec1e59c4df5ccb95d90106b
Daniele Valentino <dava95@libero.it>
2025-07-13 17:52:33 +0200

    Initial Commit

---
+++ LetHimCook/LetHimCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetHimCookApp.swift
+//  LetHimCook
+//
+//  Created by Daniele Valentino on 13/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LetHimCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LetHimCookTests/LetHimCookTests.swift
@@ -0,0 +1,17 @@
+//
+//  LetHimCookTests.swift
+//  LetHimCookTests
+//
+//  Created by Daniele Valentino on 13/07/25.
+//
+
+import Testing
+@testable import LetHimCook
+
+struct LetHimCookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
