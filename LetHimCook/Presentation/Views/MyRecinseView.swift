ae7485738106678905ab77dc0a49d02bf2ab0bab
Daniele Valentino <58222709+danieleV9@users.noreply.github.com>
2025-07-26 14:19:48 +0200

    feat: add bottom toolbar

---
+++ LetHimCook/Presentation/Views/MyRecinseView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct MyRecinseView: View {
+    var body: some View {
+        Color(.systemBackground)
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    MyRecinseView()
+}
